00b818c5f385178e5ed9858cb6066e1f0987894d
unknown <jonathanwsy@hotmail.com>
2019-02-11 14:00:29 +0000

    Almost final

---
+++ src/maths_parser.tab.hpp
@@ -0,0 +1,102 @@
+/* A Bison parser, made by GNU Bison 2.4.2.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989-1990, 2000-2006, 2009-2010 Free Software
+   Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* "%code requires" blocks.  */
+
+/* Line 1685 of yacc.c  */
+#line 1 "src/maths_parser.y"
+
+  #include "ast.hpp"
+
+  #include <cassert>
+
+  extern const Expression *g_root; // A way of getting the AST out
+
+  //! This is to fix problems when generating C++
+  // We are declaring the functions provided by Flex, so
+  // that Bison generated code can call them.
+  int yylex(void);
+  void yyerror(const char *);
+
+
+
+/* Line 1685 of yacc.c  */
+#line 55 "src/maths_parser.tab.hpp"
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     T_TIMES = 258,
+     T_DIVIDE = 259,
+     T_PLUS = 260,
+     T_MINUS = 261,
+     T_EXPONENT = 262,
+     T_LBRACKET = 263,
+     T_RBRACKET = 264,
+     T_LOG = 265,
+     T_EXP = 266,
+     T_SQRT = 267,
+     T_NUMBER = 268,
+     T_VARIABLE = 269
+   };
+#endif
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+{
+
+/* Line 1685 of yacc.c  */
+#line 17 "src/maths_parser.y"
+
+  const Expression *expr;
+  double number;
+  std::string *string;
+
+
+
+/* Line 1685 of yacc.c  */
+#line 94 "src/maths_parser.tab.hpp"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+
